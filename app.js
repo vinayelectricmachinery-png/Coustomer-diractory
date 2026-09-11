13b34a68294da54636622155d9aa256d170fa3a1
vinayelectricmachinery-png <vinayelectricmachinery@gmail.com>
2026-09-11 23:49:28 +0530

    Update Supabase URL and publishable key

--- app.js
+++ app.js
@@ -11,8 +11,8 @@
 // DO NOT use the Secret/service_role key in a browser app.
 // ------------------------------------------------------------
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE";
-const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_SUPABASE_PUBLISHABLE_KEY_HERE";
+const SUPABASE_URL = "https://rzomjnhypjcpeyxgfwht.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_8BOilVZ4Rp_BlSqWqWlcUQ_oF2k-9jV";
 
 let db = null;
 let currentShop = "Shop 1";
